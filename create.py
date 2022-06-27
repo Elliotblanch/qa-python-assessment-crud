af17072037799805a530c554e19e2fb94ebe5929
Elliot <elliotblanch@gmail.com>
2022-06-27 11:44:59 +0000

    Added sample entries to Create.py

---
+++ create.py
@@ -0,0 +1,20 @@
+from application import db
+from application.models import player, party
+from application import __init__
+
+db.drop_all()
+db.create_all()
+
+sample_player = player(
+    name = "Shen Shuhan",
+    charClass = "Fighter",
+    level = 5,
+    hp = 55,
+    ac = 16,
+    caster = True,
+    partyID = 1
+)
+
+sample_party = party(
+    name = "dynasty"
+)
