a314947d3c1b3a73c0e3c873a52a39b15950fea8
Elliot <elliotblanch@gmail.com>
2022-06-29 10:46:40 +0000

    Updated test structure

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,12 @@
-
+from application import models, db, app
+from application.models import Player, Party
+from flask_testing import TestCase
+from flask import url_for
 
 class TestBase(TestCase):
     def create_app(self):
 
-        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///test.db",
                 SECRET_KEY='TEST_SECRET_KEY',
                 DEBUG=True,
                 WTF_CSRF_ENABLED=False
@@ -13,6 +16,9 @@ class TestBase(TestCase):
     def setUp(self):
         db.create_all()
        
+        sample_party = Party(
+            name = "Dynasty"
+        )
         sample_player = Player(
             name = "Shen Shuhan",
             charClass = "Fighter",
@@ -22,9 +28,15 @@ class TestBase(TestCase):
             caster = True,
             partyID = 1
         )
+        db.session.add(sample_party)
         db.session.add(sample_player)
         db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+class TestViews(TestBase):
+    def test_home_get(self):
+        response = self.client.get(url_for('index'))
+        self.assertEqual(response.status_code, 200)
